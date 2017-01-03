90bc7e311a047de8e39ae6226851412d15a726f8
Júlia <Jlia>
2017-01-02 23:07:40 -0200

    Second commit

--- TCC/TCC/src/xml/Main.java
+++ TCC/TCC/src/xml/Main.java
@@ -1,14 +1,19 @@
 package xml;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main {
 	
-	public static final ReadFile rF = new ReadFile();
+	public static final ReadXMLFile rF = new ReadXMLFile();
 	public static final File folder = new File("/Users/Julia/Documents/TCC/");
-
+	public static ArrayList<File> receiveArq = new ArrayList<File>();
+	
 	public static void main(String[] args) {
-		rF.accessDirectory(folder);
+		receiveArq = rF.accessDirectory(folder);
+		for(int i = 0; i <= receiveArq.size(); i++){
+			rF.readTheFiles(receiveArq.get(i));			
+		}
 	}
 
 }
